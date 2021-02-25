29119bdb884bb9d4a7a1b372b9788a738cfd3d07
Marco <maalanis@miners.utep.edu>
2021-02-25 14:39:45 -0700

    added space_char and non_space_char methods

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include "tokenizer.h"
 
 int main()
 {
@@ -9,4 +9,23 @@ int main()
   printf("Enter string\n");
   scanf("%[^\n]", &z);
   printf("You have entered %s\n", &z);
+}
+
+int space_char(char c)
+{
+  if (c == 32 || c == 9|| c == 48){
+    return 1;
+  }
+  return 0;
+
+}
+int non_space_char(char c)
+{
+  if (c == 32 || c == 9 || c == 48)
+    {
+      return 0;
+    }
+  return 1;
+
+
 }
