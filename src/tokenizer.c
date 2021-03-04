2ab13de84f1fa29eec3a923efe2694f14127d2f4
Marco <maalanis@miners.utep.edu>
2021-03-04 16:45:07 -0700

    added compare method to check on strings entered

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -134,3 +134,15 @@ char **tokenize(char *str)
   tokens[i] = terminator;
   return tokens;
 }
+int compare (char *str1, char *str2)
+{
+  int i =0;
+  while(str1[i] == str2[i])
+    {
+      if(str1[i]== '\0' && str2[i] == '\0')
+	break;
+
+      i++;
+    }
+  return str1[i] - str2[i];
+}
